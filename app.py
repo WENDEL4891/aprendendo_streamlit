8f8f24185cd38a25e2979822f297e3306d342e1d
Wendel <wendel4891@gmail.com>
2026-01-07 22:26:17 -0300

    Initial commit: Streamlit structure.

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# Elementos de texto básicos
+st.title("Dashboard de Controle")
+st.header("Seção de Análise")
+st.write("Esta é uma aplicação funcional básica utilizando Streamlit.")
